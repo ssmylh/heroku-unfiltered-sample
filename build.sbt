da6104684584d7477801ea9fb732d71cd261c847
ssmylh <ssmylh@gmail.com>
2012-04-14 14:13:06 +0900

    init

---
+++ build.sbt
@@ -0,0 +1,17 @@
+organization := "com.github.ssmylh"
+
+name := "heroku-unfiltered-sample"
+
+scalaVersion := "2.9.1"
+
+version := "0.1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  "net.databinder" %% "unfiltered-filter" % "0.6.1",
+  "net.databinder" %% "unfiltered-jetty" % "0.6.1",
+  "org.clapper" %% "avsl" % "0.3.8"
+)
+
+resolvers ++= Seq(
+  "typesafe" at "http://repo.typesafe.com/typesafe/releases/"
+)
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,21 @@
+import unfiltered.request._
+import unfiltered.response._
+import org.clapper.avsl.Logger
+
+class Sample extends unfiltered.filter.Plan {
+	val logger = Logger(classOf[Sample])
+	def intent = {
+		case Path(Seg(p :: Nil)) => {
+			logger.debug(p)
+			ResponseString(p)
+		}
+		case _ => ResponseString("Hello.")
+	}
+}
+
+object Main extends App {
+	val logger = Logger(Main.getClass)
+	val port = util.Properties.envOrElse("PORT", "8080").toInt
+	logger.info("started on port:%d".format(port))
+	unfiltered.jetty.Http(port).plan(new Sample).run()
+}
